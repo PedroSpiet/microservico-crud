3c0818b40191297c408caae2d008eeaeee5f6709
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-03-18 17:18:18 -0300

    RabbitMQ parte 1

---
+++ src/main/java/com/spiet/crud/config/MessageConfig.java
@@ -0,0 +1,28 @@
+package com.spiet.crud.config;
+
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Configuration
+public class MessageConfig {
+
+    @Value("${crud.rabbitmq.exchange}")
+    String exchange;
+
+    @Bean
+    public Exchange declareExchange() {
+        return ExchangeBuilder.directExchange(exchange).build();
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
---
+++ src/main/java/com/spiet/crud/messages/ProductSendMessage.java
@@ -0,0 +1,27 @@
+package com.spiet.crud.messages;
+
+import com.spiet.crud.data.vo.ProductVO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductSendMessage {
+    @Value("${crud.rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${crud.rabbitmq.routingkey}")
+    String routingKey;
+
+    public final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ProductSendMessage(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(ProductVO productVO) {
+        rabbitTemplate.convertAndSend(exchange, routingKey ,productVO);
+    }
+}
